a1109c7f213672845c055329edd5319b85ccea20
Peter J-R <petruspop@hotmail.com>
2023-04-17 13:12:21 +0200

    Update streamlit_app.py
    Added an input box for dynamic fruit info

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,10 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response, Lesson 9
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') # Input box asking for fruit
+streamlit.write('The user entered ', fruit_choice) # Output/confirm the input
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json()) # old stuff, removed
 
 # normalize the json response 
