2342006aa007bae12bb02fdbcb03444cf395c663
Peter J-R <petruspop@hotmail.com>
2023-04-17 14:48:44 +0200

    Update streamlit_app.py
    Added a streamlit stop and moved import statements to the top

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import requests
+import pandas
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Mom\'s New healthy Diner')
 
@@ -10,7 +14,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -24,7 +27,6 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') # Input box asking for fruit
 streamlit.write('The user entered ', fruit_choice) # Output/confirm the input
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json()) # old stuff, removed
 
@@ -33,8 +35,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Output as a table
 streamlit.dataframe(fruityvice_normalized)
 
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
 # Lesson 12 Snowflake
-import snowflake.connector
 
 # Test snowflake connection
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
